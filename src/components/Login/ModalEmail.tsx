75c2ce47bd092b6672de4ad745a227911f4b6204
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-24 01:09:27 -0600

    Add loading state to ModalEmail component: integrate framer-motion for button animations and update button text based on loading status for improved user feedback.

--- src/components/Login/ModalEmail.tsx
+++ src/components/Login/ModalEmail.tsx
@@ -1,10 +1,13 @@
+import { motion } from "framer-motion";
+
 export interface ModalEmailProps {
     email: string;
     handleChangeEmail: (e: React.ChangeEvent<HTMLInputElement>) => void;
     handleSendCode: (e: React.FormEvent) => void;
+    isLoading: boolean;
 }
 
-export default function ModalEmail({ email, handleChangeEmail, handleSendCode }: ModalEmailProps) {
+export default function ModalEmail({ email, handleChangeEmail, handleSendCode, isLoading }: ModalEmailProps) {
     return (
         <>
             <h2 className="text-2xl font-bold text-slate-100">Recuperar contraseña</h2>
@@ -19,9 +22,16 @@ export default function ModalEmail({ email, handleChangeEmail, handleSendCode }:
                     className="rounded-lg border border-slate-800 bg-slate-900 px-3 py-2.5 text-sm text-white outline-none focus:border-blue-500"
                 />
 
-                <button type="submit" className="rounded-lg bg-blue-800 py-2 font-semibold text-white hover:bg-blue-900">
-                    Enviar código
-                </button>
+                <motion.button
+                    initial={{ scale: 0.9, opacity: 0, y: 40, transition: { duration: 0.3 } }}
+                    animate={{ scale: 1, opacity: 1, y: 0, transition: { duration: 0.3 } }}
+                    whileHover={{ scale: 0.9, transition: { duration: 0.2 } }}
+                    whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
+                    type="submit"
+                    className="cursor-pointer rounded-lg bg-blue-800 py-2 font-semibold text-white hover:bg-blue-900"
+                >
+                    {isLoading ? "Enviando..." : "Enviar código"}
+                </motion.button>
             </form>
         </>
     );
