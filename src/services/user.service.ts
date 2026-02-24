bf6101de0a1785d992ad249da7040cce82e73132
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-24 00:11:42 -0600

    Remove CambiarContrasena function from user.service.ts: streamline user service by eliminating unused password change functionality.

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { API_URL_USER, API_URL_PASSWORD } from "../config";
+import { API_URL_USER } from "../config";
 import type { User } from "../types/user.types";
 import { getUserErrorMessage, handleApiError } from "../utils/errores";
 
@@ -71,26 +71,6 @@ export async function EliminarUsuario({ id_usuario }: { id_usuario: string }): P
         const response = await fetch(`${API_URL_USER}/${id_usuario}`, {
             method: "DELETE",
             credentials: "include",
-        });
-
-        await handleApiError(response);
-
-        const { message } = await response.json();
-        return { message };
-    } catch (e) {
-        const errorMessage = getUserErrorMessage(e);
-        throw new Error(errorMessage);
-    }
-}
-
-export async function CambiarContrasena({ id_usuario, contrasena }: { id_usuario: string; contrasena: string }): Promise<{ message: string }> {
-    try {
-        const response = await fetch(`${API_URL_PASSWORD}/reset/${id_usuario}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ contrasena }),
         });
 
         await handleApiError(response);
