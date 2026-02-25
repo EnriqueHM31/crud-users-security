008a15de4f092bcb8536e032bd8b06ed3e8c1cfb
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-24 22:19:31 -0600

    Add useTaskPage hook: implement task management functionality including fetching, creating, updating, and deleting tasks, along with modal handling for task deletion to enhance task page capabilities.

---
+++ src/hooks/useTaskPage.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { useAuthenticatedUser } from "./useUsers";
+import { useTaskStore } from "../store/task.store";
+import { useOpen } from "./useOpen";
+import type { Task } from "../types/task.types";
+
+export function useTaskPage() {
+    const user = useAuthenticatedUser();
+    const { fetchTasks, tasks, createTask, updateTask, deleteTask } = useTaskStore();
+    const [taskEliminated, setTaskEliminated] = useState<Task | null>(null);
+
+    const openModalCreateTask = useOpen();
+    const openModalDeleteTask = useOpen();
+
+    useEffect(() => {
+        if (!user) return;
+        void fetchTasks(user.id_usuario);
+    }, [fetchTasks, user]);
+
+    const handleSubmitCompleteTask = async ({ id_tarea }: { id_tarea: string }) => {
+        await updateTask(id_tarea, true);
+    };
+
+    const handleSubmitDeleteTask = async () => {
+        if (!taskEliminated) return;
+
+        await deleteTask(taskEliminated.id_tarea);
+        handleTaskDeleted();
+    };
+
+    const handleTaskDeletedModal = (task: Task | null) => {
+        openModalDeleteTask.open();
+        setTaskEliminated(task);
+    };
+
+    const handleTaskDeleted = () => {
+        openModalDeleteTask.close();
+        setTaskEliminated(null);
+    };
+
+    return {
+        tasks,
+        createTask,
+        updateTask,
+        deleteTask,
+        handleSubmitCompleteTask,
+        handleSubmitDeleteTask,
+        handleTaskDeletedModal,
+        handleTaskDeleted,
+        openModalCreateTask,
+        openModalDeleteTask,
+        taskEliminated,
+    };
+}
